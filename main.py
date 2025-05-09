48030f0b90670c86a8e561df6120932bd74c430c
Splutterwit <98063764+delectable-worm@users.noreply.github.com>
2025-05-09 23:57:24 +0000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import sqlite3
+con = sqlite3.connect("data.db")
+
+cur = con.cursor()
+
+cur.execute("CREATE TABLE chore(id INT PRIMARY KEY, name VARCHAR(255), description TEXT, type VARCHAR(255))")
+cur.execute("CREATE TABLE people(id INT PRIMARY KEY, name VARCHAR(255) NOT NULL)")
+cur.execute("CREATE TABLE chore_instances(id INT PRIMARY KEY, chore_id int)")
 
