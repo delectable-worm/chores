728234cb3b5be2437f7b349f73a01a8dcfec34c4
Honey Raut <hraut.au@gmail.com>
2025-05-10 13:38:56 +1000

    analytics

--- unpaid-labour-tracker/src/Analytics.jsx
+++ unpaid-labour-tracker/src/Analytics.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
 import { useNavigate } from 'react-router-dom'
 export default function Analytics() {
+  const [chores, setChores] = useState([])
+
+  useEffect(() => {
+    axios.get('https://localhost:8000/chores')
+    .then(response => {
+      setChores(response.data)
+    })
+    .catch(error => {
+      console.error('Error fetching chores', error)
+    })
+  }, [])
+
   const navigate = useNavigate()
   const goToDash = () => {
     navigate('/dashboard')
@@ -12,6 +27,7 @@ export default function Analytics() {
       <p>in your household u contribute to 30% of the chores. this is a good amount</p><br></br>
       <p>see a better breakdown of the chores below</p>
       <br></br>
+    
       <button onClick={goToDash}>Back to Task Logger</button>
     </div>
   )
